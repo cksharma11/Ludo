8ba2966059eece99f84135601a2cf8a56e9a8ccb
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-05-04 15:14:11 +0530

    Styled a bit more : not sure it's good or !

--- view/script/game.js
+++ view/script/game.js
@@ -1,6 +1,30 @@
+const coin = (color) => {
+  return `<div class="coin_symbol dark_${color}" />`;
+};
+
 const createGameView = gameData => {
   const playerName = gameData.players[0].name;
   document.getElementById("player_name").innerText = playerName;
+
+  document.getElementById("red_coin_1").innerHTML = coin("red");
+  document.getElementById("red_coin_2").innerHTML = coin("red");
+  document.getElementById("red_coin_3").innerHTML = coin("red");
+  document.getElementById("red_coin_4").innerHTML = coin("red");
+
+  document.getElementById("blue_coin_1").innerHTML = coin("blue");
+  document.getElementById("blue_coin_2").innerHTML = coin("blue");
+  document.getElementById("blue_coin_3").innerHTML = coin("blue");
+  document.getElementById("blue_coin_4").innerHTML = coin("blue");
+
+  document.getElementById("green_coin_1").innerHTML = coin("green");
+  document.getElementById("green_coin_2").innerHTML = coin("green");
+  document.getElementById("green_coin_3").innerHTML = coin("green");
+  document.getElementById("green_coin_4").innerHTML = coin("green");
+
+  document.getElementById("yellow_coin_1").innerHTML = coin("yellow");
+  document.getElementById("yellow_coin_2").innerHTML = coin("yellow");
+  document.getElementById("yellow_coin_3").innerHTML = coin("yellow");
+  document.getElementById("yellow_coin_4").innerHTML = coin("yellow");
 };
 
 const getGame = () => {
@@ -8,3 +32,5 @@ const getGame = () => {
     .then(res => res.json())
     .then(res => createGameView(res));
 };
+
+window.onload = getGame;
