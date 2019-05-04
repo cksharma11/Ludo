fa009e5d5b71acdf48de6ca1d4565ba07dc2ccf4
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-05-04 15:26:53 +0530

    Create setup for initial display of game

--- handler/handler.js
+++ handler/handler.js
@@ -4,7 +4,12 @@ const dice = require("../model/dice");
 const Player = require("../model/player");
 
 const getGame = async (req, res) => {
-  const players = [new Player("Ankit", "red")];
+  const players = [
+    new Player("Ankit", "red"),
+    new Player("Chandan", "green"),
+    new Player("Dheeraj", "yellow"),
+    new Player("Gaurav", "blue")
+  ];
   const turnManager = new TurnManager(players);
   res.send(new Game(players, dice, turnManager));
 };
--- view/script/game.js
+++ view/script/game.js
@@ -1,4 +1,4 @@
-const coin = (color) => {
+const coin = color => {
   return `<div class="coin_symbol dark_${color}" />`;
 };
 
@@ -6,25 +6,13 @@ const createGameView = gameData => {
   const playerName = gameData.players[0].name;
   document.getElementById("player_name").innerText = playerName;
 
-  document.getElementById("red_coin_1").innerHTML = coin("red");
-  document.getElementById("red_coin_2").innerHTML = coin("red");
-  document.getElementById("red_coin_3").innerHTML = coin("red");
-  document.getElementById("red_coin_4").innerHTML = coin("red");
-
-  document.getElementById("blue_coin_1").innerHTML = coin("blue");
-  document.getElementById("blue_coin_2").innerHTML = coin("blue");
-  document.getElementById("blue_coin_3").innerHTML = coin("blue");
-  document.getElementById("blue_coin_4").innerHTML = coin("blue");
-
-  document.getElementById("green_coin_1").innerHTML = coin("green");
-  document.getElementById("green_coin_2").innerHTML = coin("green");
-  document.getElementById("green_coin_3").innerHTML = coin("green");
-  document.getElementById("green_coin_4").innerHTML = coin("green");
-
-  document.getElementById("yellow_coin_1").innerHTML = coin("yellow");
-  document.getElementById("yellow_coin_2").innerHTML = coin("yellow");
-  document.getElementById("yellow_coin_3").innerHTML = coin("yellow");
-  document.getElementById("yellow_coin_4").innerHTML = coin("yellow");
+  gameData.players.forEach(player => {
+    for (let coinNumber = 1; coinNumber < 5; coinNumber++) {
+      document.getElementById(`${player.color}_coin_${coinNumber}`).innerHTML = coin(
+        player.color
+      );
+    }
+  });
 };
 
 const getGame = () => {
